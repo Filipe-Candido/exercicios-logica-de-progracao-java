13563adc1b0a5400da05f745d533b217df8b20ec
filipe <filipe.candido.rodrigues@gmail.com>
2024-09-23 22:30:43 -0300

    finalizando primeiro exercicio

--- SomarNumero/src/SomarNumero.java
+++ SomarNumero/src/SomarNumero.java
@@ -1,2 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class SomarNumero {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Infomre o primeiro numero:");
+        int numero1 = scan.nextInt();
+
+        System.out.println("Informe o segundo numero");
+        int numero2 = scan.nextInt();
+
+        int soma =numero1 + numero2;
+        int multiplicacao = soma * numero1;
+        System.out.println("Resultado:"+multiplicacao);
+
+        scan.close();
+
+    }
 }
+
+
+/*
+Faça um algoritmo para somar dois números
+e multiplicar o resultado pelo primeiro número.
+*/
