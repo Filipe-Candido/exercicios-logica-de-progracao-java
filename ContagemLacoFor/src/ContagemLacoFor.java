2d44c78dabd683576c87c6908bd927f72b344017
filipe <filipe.candido.rodrigues@gmail.com>
2024-10-03 21:02:13 -0300

    Finalizado exercicio laço for contagem de 1 até 100

---
+++ ContagemLacoFor/src/ContagemLacoFor.java
@@ -0,0 +1,22 @@
+public class ContagemLacoFor {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 100 ;i++){
+            System.out.println(i);
+            if (i % 10 == 0){
+                System.out.println("Multiplo de 10 "+i);
+            }
+        }
+    }
+}
+
+
+
+/*
+Seção 7 - Exercício 2
+
+Faça um algoritmo que conte de 1 a 100 e a cada múltiplo de 10 emita
+uma mensagem: "Múltiplo de 10"
+*/
