b80b01bbc08ae8f57b237c623d8c419eee920e99
filipe <filipe.candido.rodrigues@gmail.com>
2024-10-22 23:43:25 -0300

    Declarando metodos recuperarSaldo e depositar e retirar

--- ContaBancaria/src/br/com/bancotestepoo/Conta.java
+++ ContaBancaria/src/br/com/bancotestepoo/Conta.java
@@ -4,7 +4,7 @@ public class Conta {
 		
 	int numero;
 	double saldo;
-	
+	int agencia;/*atributo agencia*/
 	
 	/*Referenciando a classe cliente dentro da classe conta, para referenciar devemos perguntar
 	 "A classe conta é um cliente ou tem um cliente*/
@@ -13,4 +13,30 @@ public class Conta {
 	Cliente cliente = new Cliente();/* declaro a classe cliente dar um nome a variavel, no caso "cliente"
 	 e trabalho dentro de um objeto cliente*/
 	
+	/*Metodo agencia recebe um atributo agencia */
+	
+	
+	
+	public void setAgencia(int agencia) {/*Metodo agencia recebe um atributo agencia */
+		this.agencia =agencia;/*O this vai sempre refenciar um parametro ou metodo do escopo da classe*/
+	
+		
+		/*Declaração de metodos em java*/
+	}public double recuperarSaldo() {
+		return saldo;
+		
+	}public void depositar(double valor) {
+		saldo =saldo +valor;
+		
+	}
+	
+	/*sobrecarga de metodos */
+	public void retirar(double valor) {
+		saldo = saldo -valor;
+		
+	}public void retirar(double valor,double taxa) {
+		saldo = saldo -valor - taxa;
+	}public void name() {
+		
+	}
 }
